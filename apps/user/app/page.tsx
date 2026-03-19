01f14ee84a532f512843af8440c6387b6c45e01d
Azazel <gd0987612345@gmail.com>
2026-03-19 10:15:57 +0100

    UI: Home lading done

--- apps/user/app/page.tsx
+++ apps/user/app/page.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+"use client";
 
-export default function Home() {
+import Hero from "./components/Home/Hero";
+import Navbar from "./components/layout/Navbar";
+import TopBar from "./components/layout/TopBar";
+
+export default function HomePage() {
   return (
-    <div>User Home</div>
-  )
+    <div className="min-h-screen bg-white">
+      <TopBar />
+      <Navbar />
+      <Hero />
+    </div>
+  );
 }
+
