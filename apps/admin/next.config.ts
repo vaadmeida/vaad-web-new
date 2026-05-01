f09faec008c92726f2c3e59290e182dc13a7415e
godwin-morwin <godwinolele10@gmail.com>
2026-05-01 06:52:40 +0100

    Turbo false

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@repo/ui"],
+  experimental: {
+    turbopack: false,
+  },
   /* config options here */
 };
 
